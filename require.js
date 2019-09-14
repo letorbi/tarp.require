3e1888b5d265b60a61afa9eb9ee3c757811ce59b
Torben <torben.letorbi@gmail.com>
2019-09-15 00:43:49 +0200

    Update license header

--- require.js
+++ require.js
@@ -1,18 +1,18 @@
 //
-// This file is part of //\ Tarp.
+//  This file is part of //\ Tarp.
 //
-// Copyright (C) 2013-2018 Torben Haase <https://pixelsvsbytes.com>
+//  Copyright (C) 2013-2019 Torben Haase <https://pixelsvsbytes.com>
 //
-// Tarp is free software: you can redistribute it and/or modify it under the
-// terms of the GNU Lesser General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option) any
-// later version.
+//  Tarp is free software: you can redistribute it and/or modify it under the
+//  terms of the GNU Lesser General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option)
+//  any later version.
 //
-// Tarp is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-// A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-// details.You should have received a copy of the GNU Lesser General Public
-// License along with Tarp.  If not, see <http://www.gnu.org/licenses/>.
+//  Tarp is distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//  FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+//  details. You should have received a copy of the GNU Lesser General Public
+//  License along with Tarp. If not, see <https://www.gnu.org/licenses/>.
 //
 ////////////////////////////////////////////////////////////////////////////////
 
